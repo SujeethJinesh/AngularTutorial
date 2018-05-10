6943eaf2b9e38af1ca19c70f83d5e9b5ca35d5a0
SujeethJinesh <sujeethjinesh@gmail.com>
2018-05-09 20:33:44 -0700

    Add Two-Way Data Binding

--- bucket-list/src/app/home/home.component.ts
+++ bucket-list/src/app/home/home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4; // allows you to surface value to ui in home.component.html
+  btnText: string = 'Add an item'
+  goalText: string = 'My first life goal' // use ngModel to do two way data binding, can be referenced as
+                                          // [(ngModel)]="goalText" in html. then can change dynamically with {{goalText}}
+
   constructor() { }
 
   ngOnInit() {
